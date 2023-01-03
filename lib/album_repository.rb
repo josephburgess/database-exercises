734e6a7b6b97c3d89727233edd356204ef295021
Joe Burgess <josephburgess@gmail.com>
2023-01-03 13:48:59 +0000

    test driving album repository class

---
+++ lib/album_repository.rb
@@ -0,0 +1,45 @@
+require_relative './album'
+class AlbumRepository
+  def all
+    sql = 'SELECT id, title, release_year FROM albums;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    albums = []
+    result_set.each do |record|
+      album = Album.new
+      album.id = record['id']
+      album.title = record['title']
+      album.release_year = record['release_year']
+
+      albums << album
+    end
+    albums
+  end
+
+  # Gets a single record by its ID
+  # One argument: the id (number)
+  def find(id)
+    sql = 'SELECT id, title, release_year FROM albums WHERE id = $1;'
+    sql_params = [id]
+    result_set = DatabaseConnection.exec_params(sql, sql_params)
+    record = result_set[0]
+    album = Album.new
+    album.id = record['id']
+    album.title = record['title']
+    album.release_year = record['release_year']
+
+    album
+    # Returns a single Student object.
+  end
+
+  # Add more methods below for each operation you'd like to implement.
+
+  # def create(student)
+  # end
+
+  # def update(student)
+  # end
+
+  # def delete(student)
+  # end
+end
---
+++ spec/album_repository_spec.rb
@@ -0,0 +1,30 @@
+require 'album_repository'
+
+RSpec.describe AlbumRepository do
+  def reset_albums_table
+    seed_sql = File.read('spec/seeds_albums.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_albums_table
+  end
+
+  it 'returns the list of albums' do
+    repo = AlbumRepository.new
+
+    albums = repo.all
+    expect(albums.length).to eq 2
+    expect(albums[1].id).to eq '2'
+    expect(albums.first.title).to eq 'Parklife'
+  end
+
+  it 'allows to search for individual ids' do
+    repo = AlbumRepository.new
+
+    album = repo.find(1)
+    expect(album.title).to eq 'Parklife'
+    expect(album.release_year).to eq '1995'
+  end
+end
