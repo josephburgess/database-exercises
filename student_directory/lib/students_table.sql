1e7e80f1414fb33d89690b6204af8e8d6cf88055
Joe Burgess <josephburgess@gmail.com>
2023-01-04 15:58:22 +0000

    adding separate cohorts table

--- student_directory/lib/students_table.sql
+++ student_directory/lib/students_table.sql
@@ -1,5 +1,31 @@
 CREATE TABLE students (
   id SERIAL PRIMARY KEY,
   name text,
-  cohort text
+  cohort_id int,
+  constraint fk_cohort foreign key(cohort_id)
+  references cohorts(id)
+  on delete cascade
 );
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY
+  name text,
+  start_date text
+)
+
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of students' names.
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of cohorts' names.
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of cohorts' starting dates.
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of students' cohorts.
