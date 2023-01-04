bc10cc9d9f5ce290bbf15a92ab6519d08a2a68a7
Joe Burgess <josephburgess@gmail.com>
2023-01-04 17:40:32 +0000

    test driving application class

--- music_library/app.rb
+++ music_library/app.rb
@@ -1,10 +1,51 @@
 require_relative 'lib/database_connection'
 require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
 
-DatabaseConnection.connect('music_library')
+class Application
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
 
-album_repository = AlbumRepository.new
+  def run
+    show 'Welcome to the music library manager!'
+    show 'What would you like to do?'
+    show '1 - List all albums'
+    show '2 - List all artists'
+    prompt_for_input
+  end
 
-album_repository.all.each do |album|
-  p "#{album.id} - #{album.title} - #{album.release_year}"
+  private
+
+  def show(message)
+    @io.puts(message)
+  end
+
+  def prompt(message)
+    @io.puts(message)
+    @io.gets.chomp
+  end
+
+  def prompt_for_input
+    input = prompt 'Enter your choice:'
+    if input == '1'
+      show 'Here is the list of albums:'
+      @album_repository.all.each do |album|
+        show "* #{album.id} - #{album.title} (#{album.release_year})"
+      end
+    else
+      show 'Here is the list of artists:'
+      @artist_repository.all.each do |artist|
+        p "* #{artist.id} - #{artist.name} (#{artist.genre})"
+      end
+    end
+  end
 end
+
+# album_repository = AlbumRepository.new
+# artist_repository = ArtistRepository.new
+# app = Application.new('music_library', Kernel, album_repository, artist_repository)
+# app.run
---
+++ music_library/spec/app_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../app'
+
+describe Application do
+  def reset_artists_table
+    seed_sql = File.read('spec/seeds_artists.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  def reset_albums_table
+    seed_sql = File.read('spec/seeds_albums.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  before :each do
+    reset_artists_table
+    reset_albums_table
+  end
+
+  it 'prompts the user with a list of options' do
+    database_name = 'music_library_test'
+    io = double(:io)
+    album_repository = AlbumRepository.new
+    artist_repository = ArtistRepository.new
+    app = Application.new(database_name, io, album_repository, artist_repository)
+    expect(io).to receive(:puts).with('Welcome to the music library manager!')
+    expect(io).to receive(:puts).with('What would you like to do?')
+    expect(io).to receive(:puts).with('1 - List all albums')
+    expect(io).to receive(:puts).with('2 - List all artists')
+    expect(io).to receive(:puts).with('Enter your choice:')
+    expect(io).to receive(:gets).and_return('1')
+    expect(io).to receive(:puts).with('Here is the list of albums:')
+    expect(io).to receive(:puts).with('* 1 - Parklife (1995)')
+    expect(io).to receive(:puts).with('* 2 - Demon Days (2005)')
+    app.run
+  end
+end
