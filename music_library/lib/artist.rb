cd0837bcf6f05b4849090e107c4925f8e5c289c1
Joe Burgess <josephburgess@gmail.com>
2023-01-04 16:33:07 +0000

    adding artist repository tests and classes

---
+++ music_library/lib/artist.rb
@@ -0,0 +1,7 @@
+class Artist
+  attr_accessor :id, :name, :genre, :albums
+
+  def initialize
+    @albums = []
+  end
+end
---
+++ music_library/spec/artist_repository_spec.rb
@@ -0,0 +1,71 @@
+require 'artist_repository'
+
+describe ArtistRepository do
+  def reset_artists_table
+    seed_sql = File.read('spec/seeds_artists.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  before :each do
+    reset_artists_table
+  end
+
+  it 'returns the list of artists' do
+    repo = ArtistRepository.new
+    artists = repo.all
+    expect(artists.length).to eq 2
+    expect(artists.first.id).to eq '1'
+    expect(artists.first.name).to eq 'Pixies'
+  end
+
+  it 'returns the Pixies as a single artist' do
+    repo = ArtistRepository.new
+    artist = repo.find(1)
+    expect(artist.name).to eq 'Pixies'
+    expect(artist.genre).to eq 'Rock'
+  end
+
+  it 'returns ABBA as a single artist' do
+    repo = ArtistRepository.new
+    artist = repo.find(2)
+    expect(artist.name).to eq 'ABBA'
+    expect(artist.genre).to eq 'Pop'
+  end
+
+  it 'creates a new artist' do
+    repo = ArtistRepository.new
+
+    artist = Artist.new
+    artist.name = 'Beatles'
+    artist.genre = 'Pop'
+
+    repo.create(artist)
+
+    artists = repo.all
+    last_artist = artists.last
+    expect(last_artist.name).to eq 'Beatles'
+    expect(last_artist.genre).to eq 'Pop'
+  end
+
+  it 'deletes an artist record' do
+    repo = ArtistRepository.new
+    id_to_delete = 1
+
+    repo.delete(id_to_delete)
+
+    all_artists = repo.all
+    expect(all_artists.length).to eq 1
+    expect(all_artists.first.id).to eq '2'
+  end
+
+  it 'deletes the two artist records' do
+    repo = ArtistRepository.new
+
+    repo.delete(1)
+    repo.delete(2)
+
+    all_artists = repo.all
+    expect(all_artists.length).to eq 0
+  end
+end
